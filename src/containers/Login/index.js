3e34b17b8a8b3a91779f08a2b07bbe81e5fbbdd7
uillasnr <wiliasreis@hotmail.com>
2023-04-05 11:08:42 -0300

    add

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,23 +1,59 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from '@hookform/resolvers/yup';
 
-import { Container, Label, Input, Button,ContainerItens } from "./styles"
+import api from "../../services/api"
+
+import { Container, Label, Input, Button, ContainerItens, SignInLink, ErrorMessage } from "./styles"
 
 
 
 function Login() {
+    /* Validação do formulario */
+    const schema = yup.object().shape({
+        email: yup.string().email('Digite um e-mail válido')
+            .required('O e-mail é obrigatório'),
+        password: yup.string().required('A senha e obrigatória')
+            .min(6, 'A senha deve ter pelo 6 digitos')
+    })
 
+    const { register, handleSubmit,
+        formState: { errors }
+    } = useForm({
+        resolver: yupResolver(schema)
+    })
 
+    const onSubmit = async clientData => {
+const response = await api.post('sessions', {
+    email: clientData.email,
+    password: clientData.password
+})
+console.log(response)
+    }
 
     return (
         <Container>
             <ContainerItens>
-                <Label>Email</Label>
-                <Input></Input>
+                <h1>Login</h1>
 
-                <Label>Senha</Label>
-                <Input></Input>
+                <form noValidate onSubmit={handleSubmit(onSubmit)}>
+                    <Label>Email</Label>
+                    <Input type="email" {...register('email')}
+                        error={errors.email?.message} />
+                    <ErrorMessage>{errors.email?.message}</ErrorMessage>
 
-                <Button>Sing up</Button>
+                    <Label>Senha</Label>
+                    <Input type="password" {...register('password')}
+                        error={errors.email?.message} />
+                    <ErrorMessage>{errors.password?.message}</ErrorMessage>
+
+                    <Button type="submit">Sing up</Button>
+                </form>
+
+                <SignInLink>
+                    Não possui conta ? <a>SignUp</a>
+                </SignInLink>
             </ContainerItens>
         </Container>
     )
--- src/containers/Login/styles.js
+++ src/containers/Login/styles.js
@@ -3,12 +3,21 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 background: #222222;
+height: 100vh;
+width: 100vw;
+display: flex;
+justify-content: center;
+align-items: center;
 
 
 `;
 export const ContainerItens = styled.div`
-display: flex;
-justify-content: center;
+background: white;
+width: 30%;
+height: 50%;
+h1{
+    color: red;
+}
 `
 
 export const Label = styled.p`
@@ -16,15 +25,65 @@ font-style: normal;
 font-weight: 500;
 font-size: 12px;
 line-height: 14px;
-color: white;
+color: red;
 margin-top: 10px;
 margin-bottom: 5px;
 `;
 
 export const Input = styled.input`
-
+width: 280px;
+height: 38.32px;
+background: #ffffff;
+box-shadow: 3px 3px 10px rgba(74, 144, 226,0.19);
+border-radius: 5px;
+border: ${props => (props.error ? ' 2px solid #cc1717' : 'none')};
+padding-left: 10px;
 `;
 
-export const Button = styled.button``;
+export const ErrorMessage = styled.p`
+font-style: normal;
+font-weight: normal;
+font-size: 14px;
+line-height: 16px;
+color: #cc1717;
+margin-top: 2px;
+`
+
+
+export const SignInLink = styled.p`
+font-style: normal;
+    font-weight: 300;
+    font-size: 10px;
+    line-height: 16px;
+    color: red;
+    margin-left: 142px;
+ a { 
+    cursor: pointer;
+    text-decoration: underline;
+}
+`
+
+export const Button = styled.button`
+ width: 280px;
+    height: 36.13px;
+    background: #36E73D;
+    border-radius: 5px;
+    border: none;
+    cursor: pointer;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    text-align: center;
+    color: #2c2b2b;
+    margin-top: 40px;
+    
+ &:hover {
+    opacity:  0.8;
+}
+&:active {
+opacity: 0.6;
+}
+`;
 
 
